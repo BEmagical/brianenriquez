3f5927b3f69625ad6059e99ffa646de970caeae5
Brian Enriquez <a.clearing.for.invention@gmail.com>
2012-10-15 16:10:28 -0700

    Removed the link on Discovery. It's not build yet.
    Signed-off-by: Brian Enriquez <a.clearing.for.invention@gmail.com>

--- wp-content/themes/starkers/index.php
+++ wp-content/themes/starkers/index.php
@@ -10,7 +10,7 @@
 	<div class="page-content row-fluid">
 	
 		<div class="page-nav span3">
-			<h2><a href="<?php bloginfo('home'); ?>">Hey, Ma!</a></h2>
+			<h2><a href="<?php bloginfo('home'); ?>">Blog</a></h2>
 			<p>“I always wanted a Mustang to rebuild its engine piece by piece. I never got one, which is why I code from scratch.”</p>
 			<ul><?php wp_list_categories('title_li='); ?></ul>
 		</div>
--- wp-content/themes/starkers/parts/nav.php
+++ wp-content/themes/starkers/parts/nav.php
@@ -1,7 +1,7 @@
 <nav class="narrow">
 	<a class="switch"><i class="icon-plus"></i> <span>Minimize</span></a>
 	<a class="blog" href="/"><i class="icon-book"></i> Blog</a>
-	<a class="discover" href="blog.dev/wp-content/themes/starkers/page-discover.php"><i class="icon-globe"></i> Discover</a>
+	<a class="discover"><i class="icon-globe"></i> Coming soon ...</a>
 	
 	<div class="clearfix"></div>
 	<div class="about-me">
