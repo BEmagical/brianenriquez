46b262700ec967469377deae16584a2cec34c59b
Brian Enriquez <a.clearing.for.invention@gmail.com>
2012-10-15 23:06:18 -0700

    Somehow got disengaged from MYSQL, reuploading the config.php file.
    Signed-off-by: Brian Enriquez <a.clearing.for.invention@gmail.com>

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define('DB_NAME', 'bemagical');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'thorsHammer');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'thorsHammer');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mmmdelicious.fatcowmysql.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
