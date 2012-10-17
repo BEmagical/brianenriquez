ecbb7f743eefef7f50b9c3ed859fdf338603b392
Brian Enriquez <a.clearing.for.invention@gmail.com>
2012-10-17 14:39:18 -0700

    Added read-more link on index.php // added retina support for susan women pink ribbon
    Signed-off-by: Brian Enriquez <a.clearing.for.invention@gmail.com>

--- wp-content/themes/starkers/parts/nav.php
+++ wp-content/themes/starkers/parts/nav.php
@@ -1,9 +1,10 @@
 <nav class="narrow">
 	<a class="switch"><i class="icon-plus"></i> <span>Minimize</span></a>
-	<a class="blog" href="/"><i class="icon-book"></i> Blog</a>
+	<a class="blog" href="/"><i class="icon-bookmark"></i> Blog</a>
 	
 	<div class="clearfix"></div>
 	<div class="about-me">
+		<p><strong>Styled pink</strong> with 3 lines of SASS in support of Breast Cancer Awareness Month. Ribbon logo courtesy of <a href="http://ww5.komen.org/" target="_blank">Susan G. Komen for the Cure.</a></p>
 		<p><strong>Powered by</strong> Wordpress and <a href="http://viewportindustries.com/products/starkers/" target="_blank">Starkers</a>, a skeleton theme by <a href="https://twitter.com/elliotjaystocks" target="_blank">@ElliotJayStocks</a>, coded on a Mac using CodeKit, Coda, Chrome, SASS, Compass, and jQuery.</p>
 		<p>Written &amp; built in Seattle <i class="icon-umbrella"></i><br />
 			&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>.<br />
