ca4ea33c3433998915235fca3a736115f2b4f76a
Brian Enriquez <a.clearing.for.invention@gmail.com>
2013-07-03 20:09:07 -0700

    Styled the single post page
    Signed-off-by: Brian Enriquez <a.clearing.for.invention@gmail.com>

--- wp-content/themes/starkers/single.php
+++ wp-content/themes/starkers/single.php
@@ -7,29 +7,23 @@
 <?php get_header(); ?>
 
 <div class="page-wrap">
+	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+		<h3 style="margin-top: 0;">The Lab</h3>
+		<h1 class="single-post-title"><?php the_title(); ?></h1>
+
 	<div class="page-content row-fluid">
-	
-		<div class="page-nav span3">
-			<h2><a href="<?php bloginfo('home'); ?>">Blog</a></h2>
-			<p>“I always wanted a Mustang to rebuild its engine piece by piece. I never got one, which is why I code from scratch.”</p>
-			<ul><?php wp_list_categories('title_li='); ?></ul>
-		</div>
+		<ul class="post-meta span2">	
+			<li><time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?></time></li>
+			<li>Brian Enriquez</li>
+			<li><?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?></li>
+		</ul>
 		
 		<div class="page-main span8">
-			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 			<article>
-			
-				<h2><span class="headline"><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>"><?php the_title(); ?></a></span></h2>
-				<div class="post-meta">
-					<span><i class="icon-time"></i> <time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?></time></span>
-					<span><i class="icon-comments"></i> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?></span>
-				</div>
 				<?php the_content(); ?>						
 				<?php comments_template( '', true ); ?>
 			</article>
 			<?php endwhile; ?>
-		</div>	
-
-	<?php get_sidebar(); ?>
+		</div>
 
 <?php get_footer(); ?>
